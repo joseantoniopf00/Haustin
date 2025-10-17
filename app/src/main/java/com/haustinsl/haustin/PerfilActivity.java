372e3115806bc1046cdb8f1f34a797b658bb9657
Jose <joseantoniopf00@gmail.com>
2025-10-18 01:18:12 +0200

    Actividades creadas

---
+++ app/src/main/java/com/haustinsl/haustin/PerfilActivity.java
@@ -0,0 +1,16 @@
+package com.haustinsl.haustin;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class PerfilActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("Estás en la actividad Perfil");
+    }
+}
